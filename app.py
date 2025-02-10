43b27ff2d30447282486314f6ce199e08f5506e8
ROHITHA SAISRI KASA <125771633+ROHITHA210@users.noreply.github.com>
2025-02-11 03:51:57 +0530

    Updated app.py nowww

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def get_predictions():
         return jsonify({"error": str(e)})
 
 
- if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
 # Run the Flask app
 #if __name__ == "__main__":
