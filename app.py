c4b7027911e07012bae76e9c2def33f5d84a7691
ROHITHA SAISRI KASA <125771633+ROHITHA210@users.noreply.github.com>
2025-02-11 10:07:58 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -72,12 +72,19 @@ def get_predictions():
     except Exception as e:
         return jsonify({"error": str(e)})
 
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Use Railway's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
+    
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
 # Run the Flask app
 #if __name__ == "__main__":
     #app.run(debug=True)
+
+
 
 
 # In[2]:
